c7128661c54ea837b8b35c754e6e410a652d41d2
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-03-17 21:39:13 -0700

    update deps

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/main.css';
-import App from './App';
+import './tailwind.css';
+import App from './features/app/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));

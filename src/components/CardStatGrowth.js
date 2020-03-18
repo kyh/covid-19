0f634071651d89df6839e850b0d126f29294ceb2
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-03-18 15:33:33 -0700

    Add growth rate

--- src/components/CardStatGrowth.js
+++ src/components/CardStatGrowth.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Card } from './Card';
 
-export const CardStatGrowth = ({ data = {}, isLoading = false }) => {
+export const CardStatGrowth = ({ data = [], isLoading = false }) => {
+  const today = data[data.length - 1];
+  const yesterday = data[data.length - 2];
+
   return (
     <Card>
       <div className="text-xs uppercase text-gray-600 mb-1 font-semibold">
         Growth Rate
       </div>
-      <div className="text-2xl">{data.total}</div>
+      {!isLoading ? (
+        <div className="text-2xl">
+          {(today.positive / yesterday.positive).toFixed(2)}
+        </div>
+      ) : null}
     </Card>
   );
 };

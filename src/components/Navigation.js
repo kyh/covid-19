71527e039a0d1113f448502c7a27a9bba4368324
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-03-19 00:43:44 -0700

    More cleanup

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,19 +7,7 @@ export const Navigation = () => {
       <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex">
-            <div className="flex-shrink-0 flex items-center">
-              <img
-                className="block lg:hidden h-8 w-auto"
-                src="/img/logos/workflow-mark-on-white.svg"
-                alt=""
-              />
-              <img
-                className="hidden lg:block h-8 w-auto"
-                src="/img/logos/workflow-logo-on-white.svg"
-                alt=""
-              />
-            </div>
-            <div className="hidden sm:ml-6 sm:flex">
+            <div className="hidden sm:flex">
               <a
                 href="#"
                 className="inline-flex items-center px-1 pt-1 border-b-2 border-indigo-500 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out"
@@ -100,73 +88,6 @@ export const Navigation = () => {
                 />
               </svg>
             </button>
-          </div>
-        </div>
-      </div>
-      <div className="hidden sm:hidden">
-        <div className="pt-2 pb-3">
-          <a
-            href="#"
-            className="block pl-3 pr-4 py-2 border-l-4 border-indigo-500 text-base font-medium text-indigo-700 bg-indigo-50 focus:outline-none focus:text-indigo-800 focus:bg-indigo-100 focus:border-indigo-700 transition duration-150 ease-in-out"
-          >
-            Dashboard
-          </a>
-          <a
-            href="#"
-            className="mt-1 block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out"
-          >
-            Team
-          </a>
-          <a
-            href="#"
-            className="mt-1 block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out"
-          >
-            Projects
-          </a>
-          <a
-            href="#"
-            className="mt-1 block pl-3 pr-4 py-2 border-l-4 border-transparent text-base font-medium text-gray-600 hover:text-gray-800 hover:bg-gray-50 hover:border-gray-300 focus:outline-none focus:text-gray-800 focus:bg-gray-50 focus:border-gray-300 transition duration-150 ease-in-out"
-          >
-            Calendar
-          </a>
-        </div>
-        <div className="pt-4 pb-3 border-t border-gray-200">
-          <div className="flex items-center px-4">
-            <div className="flex-shrink-0">
-              <img
-                className="h-10 w-10 rounded-full"
-                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                alt=""
-              />
-            </div>
-            <div className="ml-3">
-              <div className="text-base font-medium leading-6 text-gray-800">
-                Tom Cook
-              </div>
-              <div className="text-sm font-medium leading-5 text-gray-500">
-                tom@example.com
-              </div>
-            </div>
-          </div>
-          <div className="mt-3">
-            <a
-              href="#"
-              className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 focus:outline-none focus:text-gray-800 focus:bg-gray-100 transition duration-150 ease-in-out"
-            >
-              Your Profile
-            </a>
-            <a
-              href="#"
-              className="mt-1 block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 focus:outline-none focus:text-gray-800 focus:bg-gray-100 transition duration-150 ease-in-out"
-            >
-              Settings
-            </a>
-            <a
-              href="#"
-              className="mt-1 block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100 focus:outline-none focus:text-gray-800 focus:bg-gray-100 transition duration-150 ease-in-out"
-            >
-              Sign out
-            </a>
           </div>
         </div>
       </div>

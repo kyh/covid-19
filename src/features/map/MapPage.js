57443e95a62a6a8b4811be76fd80830ddd9c0843
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-03-23 18:45:36 -0700

    Add loader

--- src/features/map/MapPage.js
+++ src/features/map/MapPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ContentLoader from 'react-content-loader';
 import { useGetStatesData } from 'hooks/useGetStatesData';
 import { PageContainer } from 'components/PageContainer';
 import { Map } from 'components/Map';
@@ -7,7 +8,11 @@ export const MapPage = () => {
   const { stateToData, totalPositives, isLoading } = useGetStatesData();
   return (
     <PageContainer>
-      {isLoading ? null : (
+      {isLoading ? (
+        <ContentLoader width="100%" height="600">
+          <rect x="0" y="0" rx="4" ry="4" width="100%" height="100%" />
+        </ContentLoader>
+      ) : (
         <Map stateToData={stateToData} totalPositives={totalPositives} />
       )}
     </PageContainer>

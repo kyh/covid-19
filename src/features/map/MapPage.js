e1dcd9c3fa7e4d10703a7e3740840d428748a3cc
Kaiyu Hsu <im.kaiyu@gmail.com>
2020-03-23 20:39:43 -0700

    Add map title

--- src/features/map/MapPage.js
+++ src/features/map/MapPage.js
@@ -8,6 +8,9 @@ export const MapPage = () => {
   const { stateToData, totalPositives, isLoading } = useGetStatesData();
   return (
     <PageContainer>
+      <div className="text-xs uppercase text-gray-600 mb-1 font-semibold">
+        Distribution across states
+      </div>
       {isLoading ? (
         <ContentLoader width="100%" height="600">
           <rect x="0" y="0" rx="4" ry="4" width="100%" height="100%" />
